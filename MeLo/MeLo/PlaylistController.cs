2a5a1d024646e195330513eef1773aa8af2747e1
psoltesz <peter.soltesz92@gmail.com>
2017-08-11 15:09:12 +0200

    added Playlist class;
    added playlistController class;
    added new dialog for adding new playlist;
    new playlists are saved into the database

---
+++ MeLo/MeLo/PlaylistController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeLo.Models;
+
+namespace MeLo
+{
+    class PlaylistController
+    {
+        private static PlaylistController instance;
+
+        private PlaylistController() { }
+
+        public static PlaylistController Setup()
+        {
+            if (instance == null)
+            {
+                instance = new PlaylistController();
+            }
+            return instance;
+        }
+
+        public List<PlaylistSet> GetPlaylists()
+        {
+            List<PlaylistSet> playlists;
+            using (var db = new MeLoDBModels())
+            {
+                playlists = db.PlaylistSet.ToList();
+            }
+            return playlists;
+        }
+
+        public void SavePlaylistToDatabase(string name)
+        {
+            using (var db = new MeLoDBModels())
+            {
+                PlaylistSet newPlaylist = new PlaylistSet();
+                newPlaylist.Name = name;
+                db.PlaylistSet.Add(newPlaylist);
+                db.SaveChanges();
+            }
+            
+
+        }
+    }
+}
