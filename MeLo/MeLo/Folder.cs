b44f768de150a41379e936e48ec777f28d30749e
vetesipeti <vetesipeti@gmail.com>
2017-08-11 15:10:49 +0200

    Function for basic fileseparation by type is created in Folder class.

--- MeLo/MeLo/Folder.cs
+++ MeLo/MeLo/Folder.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MeLo
 {
@@ -10,11 +12,52 @@ namespace MeLo
     {
         public string Name { get; set; }
         public string Path { get; set; }
+        private DirectoryInfo currentDirectoryInfo;
 
         public Folder(string name, string path)
         {
             Name = name;
             Path = path;
+            currentDirectoryInfo = new DirectoryInfo(Path);
+        }
+
+        private DirectoryInfo[] GetSubdirectories()
+        {
+            DirectoryInfo[] subDirectories;
+            try
+            {
+                subDirectories = currentDirectoryInfo.GetDirectories();
+                return subDirectories;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access denied. You might not see every folder you need.", "Error");
+                subDirectories = new DirectoryInfo[0];
+                return subDirectories;
+            }
+        }
+
+        private FileInfo[] GetFiles()
+        {
+            FileInfo[] files;
+            try
+            {
+                files = currentDirectoryInfo.GetFiles();
+                return files;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access denied. You might not see every file you need.", "Error");
+                files = new FileInfo[0];
+                return files;
+            }
+        }
+
+        public void ListContent()
+        {
+
         }
     }
+    
+
 }
